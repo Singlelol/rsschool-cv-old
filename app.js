406a4f0c22c868df164c5cc4ce91bf547c98976d
Ivan Salov <ivan.v.salov@gmail.com>
2022-06-08 00:16:03 +0300

    fix: edit adaptive style.css

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-let topBtn = document.querySelector('.return_button');
+let topBtn = document.querySelector('.return_button')
 
 function toTop() {
   if (window.pageYOffset > 20) {
     topBtn.style.opacity = '1'
   } else { topBtn.style.opacity = '0' }
-};
+}
 
 topBtn.onclick = function () {
 	window.scrollTo(0,0)
-};
+}
 
-window.onscroll = toTop;
+window.onscroll = toTop
