b89ae4b324082674ca61545026428f8022b42c1f
Ivan Salov <ivan.v.salov@gmail.com>
2022-06-08 00:53:38 +0300

    fix: edit app.js

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 let topBtn = document.querySelector('.return_button')
 
-function toTop() {
-  if (window.pageYOffset > 20) {
-    topBtn.style.opacity = '1'
-  } else { topBtn.style.opacity = '0' }
-}
-
 topBtn.onclick = function () {
 	window.scrollTo(0,0)
 }
 
-window.onscroll = toTop
+document.addEventListener('scroll', e => {
+  if (window.pageYOffset > 200) {
+    topBtn.style.opacity = '1'
+  } else { topBtn.style.opacity = '0' }
+})
